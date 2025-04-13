ea4235b62eabcdc1cb32507bc9016fba9c6fa702
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-04-13 12:01:15 -0300

    create exercicio02

--- Exercicio01.py
+++ Exercicio01.py
@@ -1,5 +1,5 @@
-#Exercício 1:
-#Crie um programa que peça o nome do usuário e imprima uma saudação.
+# Exercício 1:
+# Crie um programa que peça o nome do usuário e imprima uma saudação.
 
 nome = input("Qual é o seu nome? ")
 print(f"Olá, {nome}! Seja bem-vindo(a) ao nosso programa.")
---
+++ Exercicio02.py
@@ -0,0 +1,6 @@
+# Exercício 2:
+# Peça a idade e mostre quantos anos a pessoa terá em 10 anos.
+
+idade = int(input("Qual é a sua idade? "))
+idade_futura = idade + 10
+print(f"Em 10 anos, você terá {idade_futura} anos.")
