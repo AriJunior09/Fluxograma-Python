3a838233f0d17b800971a878607bd46928f50f5f
arijunior09 <arioliveirajuniorjose@gmail.com>
2025-08-27 22:15:13 -0300

    update

---
+++ 1-Fundamentos/1-Sintaxe/Exercicios/abrirImagem.py
@@ -0,0 +1,11 @@
+import os
+print("Diretório atual:", os.getcwd())
+
+from PIL import Image
+
+img = Image.open('1-Fundamentos/1-Sintaxe/Exercicios/Logo.png')
+
+
+img.show()
+
+#C:\repositorios\fluxograma-Python\1-Fundamentos\1-Sintaxe\Exercicios
